e32d06ae264c71e40a7d9f5be1c73c4cbe97e955
austinerb <austin.erb8@gmail.com>
2021-11-22 20:18:16 -0500

    finished project

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// get form
+let form = document.querySelector("form");
+let email = form.querySelector("input");
+
+// add event listener
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if(isValidEmail()) {
+    form.classList.remove("error");
+
+    form.submit();
+  } else {
+    form.classList.add("error");
+  }
+});
+
+function isValidEmail() {
+  let regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  return regex.test(email.value);
+}
